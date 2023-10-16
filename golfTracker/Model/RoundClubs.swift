dae68e9a7dda767c5bf847d584c363007f74c4f8
ndubauskas <ndubauskas95@yahoo.com>
2023-10-16 12:09:03 -0700

    add extra strucs

---
+++ golfTracker/Model/RoundClubs.swift
@@ -0,0 +1,16 @@
+//
+//  RoundClubs.swift
+//  golfTracker
+//
+//  Created by Nick Dubauskas on 10/16/23.
+//
+
+import Foundation
+
+struct RoundClubs{
+    let name = ""
+    var avgDistance = 0.0
+    var amountHit = 0
+    var longestDistance = 0.0
+    
+}
